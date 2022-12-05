b27c85c78dc7ab4cfe22f53334f7653a6c2e6ec5
Sam Cater <sam@samcater.com>
2022-12-05 15:45:32 +0000

    Jank removed

--- sam/day1/day1.py
+++ sam/day1/day1.py
@@ -1,12 +1,11 @@
-count=0
 total=0
 calories_list=[]
 calories_file = open('calories.txt')
 for line in calories_file:
     if line in ['\n', '\r\n']:
+        calories_list.append(total)
         total=0
     else:
         total=total+int(line)
-        calories_list.append(total)
 print("Most Calories is ", max(calories_list))
 calories_file.close()
